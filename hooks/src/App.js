b43b31336525bd4930debf79d6c3ea4ca2976da3
sirkevinc <16568166+sirkevinc@users.noreply.github.com>
2019-07-02 01:03:06 -0400

    useState for a form

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import { useForm } from './useForm';
 
-function App() {
+const App = () => {
+  // const [{count, count2}, setCount] = useState({ count: 10, count2: 20 });
+  // first value is value of state
+  // second value is function that updates state
+
+  // const [count, setCount] = useState(10);
+  // const [count2, setCount2] = useState(20);
+
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
+
+  const [values, handleChange] = useForm({email: '', password: ''});
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <button onClick={() => setCount(currentState => ({ ...currentState, count: currentState.count + 1 }))}>+</button> */}
+      {/* <button onClick={() => {
+        setCount(c => c + 1);
+        setCount2(c => c + 1);
+      }}>
+        +
+      </button>
+      <div>count 1: {count}</div>
+      <div>count 2: {count2}</div> */}
+      
+      {/* <input name='email' value={email} onChange={e => setEmail(e.target.value)} />
+      <input type='password' value={password} name='password' onChange={e => setPassword(e.target.value)}/> */}
+      
+      <input name='email' value={values.email} onChange={handleChange} />
+      <input type='password' value={values.password} name='password' onChange={handleChange} />
     </div>
-  );
+  )
 }
 
 export default App;
