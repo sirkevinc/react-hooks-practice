69356c06c93ad348e94d5579979dba730d1d56aa
SirKevinC <16568166+sirkevinc@users.noreply.github.com>
2019-07-13 14:58:55 -0400

    useEffect

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,5 +1,6 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useForm } from './useForm';
+import { useFetch } from './useFetch';
 
 const App = () => {
   // const [{count, count2}, setCount] = useState({ count: 10, count2: 20 });
@@ -12,10 +13,33 @@ const App = () => {
   // const [email, setEmail] = useState('');
   // const [password, setPassword] = useState('');
 
-  const [values, handleChange] = useForm({email: '', password: ''});
+  const [values, handleChange] = useForm({email: '', password: '', firstName: ''});
+
+  const [count, setCount] = useState(() => JSON.parse(localStorage.getItem('count')));
+
+  const { data, loading } = useFetch(`http://numbersapi.com/${count}/trivia`)
+
+  useEffect (() => {
+    localStorage.setItem('count', JSON.stringify(count));
+  }, [count])
+
+  // useEffect(() => {
+  //   const onMouseMove = e => {
+  //     console.log(e);
+  //   };
+  //   window.addEventListener('mousemove', onMouseMove);
+
+  //   return () => {
+  //     window.removeEventListener('mousemove', onMouseMove)
+  //   }
+
+  // }, []);
   
   return (
     <div>
+      <div>{!data ? "loading..." : data}</div>
+      <div>count: {count}</div>
+      <button onClick={() => setCount(c => c+ 1)}>increment</button>
       {/* <button onClick={() => setCount(currentState => ({ ...currentState, count: currentState.count + 1 }))}>+</button> */}
       {/* <button onClick={() => {
         setCount(c => c + 1);
@@ -29,8 +53,9 @@ const App = () => {
       {/* <input name='email' value={email} onChange={e => setEmail(e.target.value)} />
       <input type='password' value={password} name='password' onChange={e => setPassword(e.target.value)}/> */}
       
-      <input name='email' value={values.email} onChange={handleChange} />
-      <input type='password' value={values.password} name='password' onChange={handleChange} />
+      <input name='email' value={values.email} onChange={handleChange} placeholder='email' />
+      <input type='password' value={values.password} name='password' onChange={handleChange} placeholder = 'password'/>
+      <input name='firstName' value={values.firstName} onChange={handleChange} placeholder='firstName' />
     </div>
   )
 }
